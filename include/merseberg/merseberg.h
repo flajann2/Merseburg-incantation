ad42056fc16c4f055b4b17b866b636a858d4fcfe
Fred Mitchell <fred.mitchell@gmx.de>
2019-04-24 19:30:38 +0200

    :bug: working out how to do tuples as varadic parameters to the funciton that will be called back on multiple threads...

--- include/merseberg/merseberg.h
+++ include/merseberg/merseberg.h
@@ -1,25 +1,41 @@
 #pragma once
 
-#include <tuple>
 #include <thread>
+#include <tuple>
 
 namespace merseberg {
   /**
    */
-  template <typename Container, class... Args> class incantation {
-    std::tuple<Args...> m_tuple;
-    
-  public:
+  template <typename Container, class... Args>
+  class incantation {
+    using args_t = std::tuple<Args...>;
+
+    // TODO: make this more flexible.
+    const std::size_t m_thread_count = (std::thread::hardware_concurrency() > 0) ? std::thread::hardware_concurrency() : 4;
+    args_t m_tuple;
+
+    void do_something_with_values() {
+      invoke_task(m_tuple, std::index_sequence_for<Args...>());
+    }
+
+    void yadda(Args... values) {}
+
+    // The helper method.
+    template <std::size_t... Is>
+    void invoke_task(const args_t& tuple,
+                     std::index_sequence<Is...>) {
+      yadda(std::get<Is>(tuple)...);
+    }
+
+   public:
     template <class Function>
-    incantation(Function&& f, Args&&... args) {
+    incantation(const Container&& c, Function&& f, Args&&... args) {
       m_tuple = std::tuple<Args...>(args...);
     }
-    
+
+    void invoke() {}
     void join() {}
     void detach() {}
   };
 
-} // namespace merse
-
-
-
+}  // namespace merseberg
--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -5,7 +5,9 @@ namespace {
   using namespace merseberg;
   
   TEST(incantation, basic_test) {
-
+    auto container = std::vector<int>{1, 1, 2, 3, 5, 8, 13};
+    auto func = []() {};
+    auto imp = incantation(container, );
   }
 }  // namespace
 

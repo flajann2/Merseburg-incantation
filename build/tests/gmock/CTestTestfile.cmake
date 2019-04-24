ad8b67f2c0c97e67b5e20ef5167d8f06ba6ed801
Fred Mitchell <fred.mitchell@gmx.de>
2019-04-24 15:16:39 +0200

    :bug: :see_no_evil:  whoops -- forgot .gitignore

--- build/tests/gmock/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/lordalveric/development/cpp_proj/merseburg-incantation/build/tests/googletest-release-1.8.0
-# Build directory: /home/lordalveric/development/cpp_proj/merseburg-incantation/build/tests/gmock
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")

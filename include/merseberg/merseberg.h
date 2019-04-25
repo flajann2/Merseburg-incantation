66471a2542733dada6824a5c441462f892cb1c88
Fred Mitchell <fred.mitchell@gmx.de>
2019-04-25 11:59:47 +0200

    :sparkles: progress onthe whole affair

--- include/merseberg/merseberg.h
+++ include/merseberg/merseberg.h
@@ -2,17 +2,26 @@
 
 #include <thread>
 #include <tuple>
+#include <vector>
 
 namespace merseberg {
   /**
    */
-  template <typename Container, class... Args>
+  template <class Container, class... Args>
   class incantation {
     using args_t = std::tuple<Args...>;
+    using value_t = typename Container::value_type;
 
     // TODO: make this more flexible.
-    const std::size_t m_thread_count = (std::thread::hardware_concurrency() > 0) ? std::thread::hardware_concurrency() : 4;
+    const std::size_t m_thread_count = (std::thread::hardware_concurrency() > 0)
+                                           ? std::thread::hardware_concurrency()
+                                           : 4;
     args_t m_tuple;
+    
+    /// compliation of all results from all threads
+    std::vector<value_t> m_results {};
+
+    const Container& m_container;
 
     void do_something_with_values() {
       invoke_task(m_tuple, std::index_sequence_for<Args...>());
@@ -22,20 +31,24 @@ namespace merseberg {
 
     // The helper method.
     template <std::size_t... Is>
-    void invoke_task(const args_t& tuple,
-                     std::index_sequence<Is...>) {
+    void invoke_task(const args_t& tuple, std::index_sequence<Is...>) {
       yadda(std::get<Is>(tuple)...);
     }
 
    public:
     template <class Function>
-    incantation(const Container&& c, Function&& f, Args&&... args) {
-      m_tuple = std::tuple<Args...>(args...);
+    incantation(const Container& c, Function&& f, Args&&... args)
+        : m_container(c), m_tuple(std::tuple<Args...>(args...)) {
+
     }
 
     void invoke() {}
     void join() {}
     void detach() {}
+    template <class Function>
+    value_t operator()(Function&& freduce) {
+      return freduce(m_results); 
+    }
   };
 
 }  // namespace merseberg
--- tests/simple_test.cpp
+++ tests/simple_test.cpp
@@ -1,13 +1,41 @@
+#include <atomic>
 #include <iostream>
 #include <merseberg>
 #include "gtest/gtest.h"
+
 namespace {
   using namespace merseberg;
-  
+  using namespace std;
+
   TEST(incantation, basic_test) {
-    auto container = std::vector<int>{1, 1, 2, 3, 5, 8, 13};
-    auto func = []() {};
-    auto imp = incantation(container, );
+    auto container = std::vector<int>{};
+    for (int i = 0; i < 1024; ++i) {
+      container.push_back(i);
+    }
+
+    std::atomic<int> sum = 0;
+
+    auto sigma = [&sum](auto begin, auto end) {
+      for (auto pi = begin; pi != end; ++pi) {
+        sum += *pi;
+      }
+      return static_cast<int>(sum);
+    };
+
+    // single threaded operation
+    auto single = sigma(container.begin(), container.end());
+    cout << "*** single = " << single << endl;
+
+    auto imp = incantation(container, sigma);
+    imp.invoke();
+    auto cocurrent = imp([](auto res) {
+      int s = 0;
+      for (auto a : res) {
+        s += a;
+      }
+      return s;
+    });
+    EXPECT_EQ(single, cocurrent);
   }
 }  // namespace
 

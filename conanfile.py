ffa00da9b70f4d6cde5374aadb717d5f22dab155
Fred Mitchell <fred.mitchell@gmx.de>
2019-05-26 09:44:35 +0200

    :art: conan

--- conanfile.py
+++ conanfile.py
@@ -9,30 +9,8 @@ class MerseburgincantationConan(ConanFile):
     url = "https://github.com/flajann2/merseburg-incantation.git"
     description = "Merseburg process collections as theads across your cores"
     topics = ("concurrency", "threads", "workers", "cores")
-    settings = "os", "compiler", "build_type", "arch"
-    options = {"shared": [True, False]}
-    default_options = "shared=False"
-    generators = "cmake"
-
-    def source(self):
-        self.run("git clone https://github.com/flajann2/merseburg-incantation.git")
-        # This small hack might be useful to guarantee proper /MT /MD linkage
-        # in MSVC if the packaged project doesn't have variables to set it
-        # properly
-        tools.replace_in_file("merseburg-incantation/CMakeLists.txt", "PROJECT(merseburg-incantation)",
-                              '''PROJECT(merseburg-incantation)
-include(${CMAKE_BINARY_DIR}/conanbuildinfo.cmake)
-conan_basic_setup()''')
-
-    def build(self):
-        cmake = CMake(self)
-        cmake.configure(source_folder="merseburg-incantation")
-        cmake.build()
-
-        # Explicit way:
-        # self.run('cmake %s/merseburg-incantation %s'
-        #          % (self.source_folder, cmake.command_line))
-        # self.run("cmake --build . %s" % cmake.build_config)
+    exports_sources = "include/*"
+    no_copy_source = True
 
     def package(self):
         self.copy("*.h", dst="include", src="merseburg-incantation")
